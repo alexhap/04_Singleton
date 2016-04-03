fbfb091e26aea0dc6875215893a7ae303543cc38
Romanenko Olexiy <hapa.go2@gmail.com>
2016-04-03 22:51:31 +0300

    Squashed commit of the following:
    commit 046f8f548180301ccfe7bee4eb67d15cf01aab07
    Author: Romanenko Olexiy <hapa.go2@gmail.com>
    Date:   Sun Apr 3 22:22:40 2016 +0300
    
        Squashed commit of the following:
    
        commit 7c8285e3824dcfafc49f42ae59d0748d998a3faa
        Author: Romanenko Olexiy <hapa.go2@gmail.com>
        Date:   Thu Mar 31 19:26:38 2016 +0300
    
            caps fix2
    
        commit 78d85d2bbd7d15f3f2ddba464880ceb0724c9426
        Author: Romanenko Olexiy <hapa.go2@gmail.com>
        Date:   Thu Mar 31 19:24:24 2016 +0300
    
            caps
    
        commit f7003e67ce34eaf47af4abd6e84fe87830d1c19b
        Author: Romanenko Olexiy <hapa.go2@gmail.com>
        Date:   Thu Mar 31 19:22:41 2016 +0300
    
            minor fix
    
        commit a0cdf54d48f1c663bdbc011bf159c14d4d80c1d7
        Author: Romanenko Olexiy <hapa.go2@gmail.com>
        Date:   Tue Feb 23 01:30:40 2016 +0200
    
            git gui test
    
        commit bd9362509ab12d4e7d0451c96b172386613e45b8
        Author: Romanenko Olexiy <hapa.go2@gmail.com>
        Date:   Tue Feb 23 01:00:21 2016 +0200
    
            comment from history :)
    
    commit 2015984024a45b994db405edc7ec1927f26c4a98
    Author: Romanenko Olexiy <hapa.go2@gmail.com>
    Date:   Tue Feb 23 00:45:14 2016 +0200
    
        comment removed
    
    commit c4638dca9cb14f2b17bab1e7b603e9a98889ceed
    Merge: 555e85f 1ed6c8b
    Author: Romanenko Olexiy <hapa.go2@gmail.com>
    Date:   Tue Feb 23 00:41:07 2016 +0200
    
        Merge remote-tracking branch 'refs/remotes/origin/banch_test'
    
        # Conflicts:
        #	src/ChokolateBoiler.java
    
    commit 555e85f636fa10da9f663054513355f84d041701
    Author: Romanenko Olexiy <hapa.go2@gmail.com>
    Date:   Tue Feb 23 00:28:53 2016 +0200
    
        no code reformat
    
    commit 1ed6c8bc19344e6c51f386d9a27c6ff7676b8d61
    Author: Romanenko Olexiy <hapa.go2@gmail.com>
    Date:   Tue Feb 23 00:25:44 2016 +0200
    
        code reformat

--- src/ChokolateBoiler.java
+++ src/ChokolateBoiler.java
@@ -1,54 +1,63 @@
 /**
  * Created by alex on 18.06.2015.
+ * for education purposes only :)
  *
  */
 
 public class ChokolateBoiler {
     private boolean empty;
-    private boolean boiled;
+    private boolean BOILed;
     private static ChokolateBoiler instanceId = new ChokolateBoiler();
 
     private ChokolateBoiler() {
         empty = true;
-        boiled = false;
+        BOILed = false;
     }
 
     private static ChokolateBoiler getInstance() {
         return instanceId;
     }
 
-    public void boil() {
+    public void BOIL() {
         if (!isEmpty() && !isBoiled()) {
-            this.boiled = true;
-            System.out.println("boil!!! boiled = " + isBoiled());
+            this.BOILed = true;
+            System.out.println("BOIL!!! BOILed = " + isBoiled());
         }
     }
+
     public void fill() {
         if (isEmpty()) {
             empty = false;
-            boiled = false;
-            System.out.printf("fill!!! boiled = %b\tempty = %b\n", isBoiled(), isEmpty());
+            BOILed = false;
+            System.out.printf("fill!!! BOILed = %b\tempty = %b\n", isBoiled(), isEmpty());
         }
     }
-    public void drain() {
+
+    public void DRAIN() {
         if (!isEmpty() && isBoiled()) {
             empty = true;
-            System.out.println("drain!!! empty = " + isEmpty());
+            System.out.println("DRAIN!!! empty = " + isEmpty());
         }
     }
-    public boolean isEmpty() { return empty; }
-    public boolean isBoiled() { return boiled; }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public boolean isBoiled() {
+        return BOILed;
+    }
 
     public static void main(String args[]) {
-        ChokolateBoiler.getInstance().drain();
-        ChokolateBoiler.getInstance().boil();
+        ChokolateBoiler.getInstance().DRAIN();
+        ChokolateBoiler.getInstance().BOIL();
         ChokolateBoiler.getInstance().fill(); // 1
         ChokolateBoiler.getInstance().fill();
-        ChokolateBoiler.getInstance().drain();
-        ChokolateBoiler.getInstance().boil(); // 2
-        ChokolateBoiler.getInstance().boil();
-        ChokolateBoiler.getInstance().drain(); // 3
-        ChokolateBoiler.getInstance().drain();
-        ChokolateBoiler.getInstance().boil();
+        ChokolateBoiler.getInstance().DRAIN();
+        ChokolateBoiler.getInstance().BOIL(); // 2
+        ChokolateBoiler.getInstance().BOIL();
+        ChokolateBoiler.getInstance().DRAIN(); // 3
+        ChokolateBoiler.getInstance().DRAIN();
+        ChokolateBoiler.getInstance().BOIL();
     }
 }
